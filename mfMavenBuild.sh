cabf7276842ca0003390cffde1b7bfdf0ede9fed
Mark <mark.aaron.fisher@gmail.com>
2017-08-17 09:28:44 -0700

    make some modifications (not yet complete) to mfMavenBuild.sh script

--- mfMavenBuild.sh
+++ mfMavenBuild.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp /usr/local/apache-tomcat-7.0.79/webapps/wildbook/tweetFind.jsp ~/Desktop/temp/Wildbook_javaTweetBot/src/main/webapp/
+cp /usr/local/apache-tomcat-7.0.79/webapps/wildbook/tweetTest.jsp ~/Desktop/temp/Wildbook_javaTweetBot/src/main/webapp/
 
 rm -rf /usr/local/apache-tomcat-7.0.79/webapps/wildbook.war
 
@@ -9,4 +10,9 @@ rm -rf /usr/local/apache-tomcat-7.0.79/webapps/wildbook
 rm -rf /usr/local/apache-tomcat-7.0.79/logs/catalina.out
 
 mvn clean install -DskipTests -Dmaven.javadoc.skip=true && cp /Users/mf/Desktop/temp/Wildbook_javaTweetBot/target/wildbook-6.0.0-EXPERIMENTAL.war /usr/local/apache-tomcat-7.0.79/webapps/wildbook.war
+
+cp ~/Desktop/temp/Wildbook_javaTweetBot/tiwtter.properties /Desktop/temp/Wildbook_javaTweetBot/target/classes/bundles/
+cp ~/Desktop/temp/Wildbook_javaTweetBot/tiwtter.properties /Desktop/temp/Wildbook_javaTweetBot//target/wildbook-6.0.0-EXPERIMENTAL/WEB-INF/classes/bundles/
+cp ~/Desktop/temp/Wildbook_javaTweetBot/tiwtter.properties /Users/mf/Applications/tomcat/webapps/wildbook/WEB-INF/classes/bundles/
+cp ~/Desktop/temp/Wildbook_javaTweetBot/tiwtter.properties /Users/mf/Applications/tomcat/webapps/wildbook_data_dir/WEB-INF/classes/bundles/twitter.properties
 
