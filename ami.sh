6b977dee4ed206a5e9e9ece1aa0ac740c7adc7a3
Whaleshark Dev <info@whaleshark.org>
2017-08-17 17:23:09 +0000

    confirm that running ami.sh as root allows for correct behavior of tweetTest.jsp

--- ami.sh
+++ ami.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 cp /home/ubuntu/Wildbook_javaTweetBot/twitter.properties /home/ubuntu/Wildbook_javaTweetBot/src/main/resources/bundles/twitter.properties
-#cp /var/lib/tomcat/webapps/wildbook/tweetFind.jsp /home/ubuntu/Wildbook_javaTweetBot/src/main/webapp/
+cp /home/ubuntu/Wildbook_javaTweetBot/twitter.properties /data/whaleshark_data_dirs/shepherd_data_dir/WEB-INF/classes/bundles/
 
-#rm -rf /var/lib/tomcat/webapps/wildbook.war
+rm -rf /var/lib/tomcat7/webapps/wildbook.war
 
-rm -rf /var/lib/tomcat/webapps/wildbook
+rm -rf /var/lib/tomcat7/webapps/wildbook
 
-rm -rf /var/lib/tomcat/logs/catalina.out
+rm -rf /var/lib/tomcat7/logs/catalina.out
 
 mvn clean install -DskipTests -Dmaven.javadoc.skip=true
-cp /home/ubuntu/Wildbook_javaTweetBot/target/wildbook-6.0.0-EXPERIMENTAL.war /var/lib/tomcat/webapps/wildbook.war
+cp /home/ubuntu/Wildbook_javaTweetBot/target/wildbook-6.0.0-EXPERIMENTAL.war /var/lib/tomcat7/webapps/wildbook.war
+cp /home/ubuntu/Wildbook_javaTweetBot/target/wildbook-6.0.0-EXPERIMENTAL/ /var/lib/tomcat7/webapps/wildbook
+sudo chmod 777 /var/lib/tomcat7/webapps/wildbook.war
 
-cd /var/lib; sudo chmod -R 777 tomcat
+cd /var/lib; sudo chmod -R 777 tomcat7/; sudo chown -R tomcat7 tomcat7/; sudo chgrp -R tomcat7 tomcat7/
 
