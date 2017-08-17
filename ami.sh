d4b343ad1fa3a2881dba8da297690a357c9fa758
Whaleshark Dev <info@whaleshark.org>
2017-08-17 20:36:27 +0000

    fix issue in ami.sh that was preventing proper copyig of the wildbook build over to tomcat

--- ami.sh
+++ ami.sh
@@ -10,9 +10,10 @@ rm -rf /var/lib/tomcat7/webapps/wildbook
 rm -rf /var/lib/tomcat7/logs/catalina.out
 
 mvn clean install -DskipTests -Dmaven.javadoc.skip=true
-cp /home/ubuntu/Wildbook_javaTweetBot/target/wildbook-6.0.0-EXPERIMENTAL.war /var/lib/tomcat7/webapps/wildbook.war
-cp /home/ubuntu/Wildbook_javaTweetBot/target/wildbook-6.0.0-EXPERIMENTAL/ /var/lib/tomcat7/webapps/wildbook
-sudo chmod 777 /var/lib/tomcat7/webapps/wildbook.war
+#cp /home/ubuntu/Wildbook_javaTweetBot/target/wildbook-6.0.0-EXPERIMENTAL.war /var/lib/tomcat7/webapps/wildbook.war
+sudo mkdir /var/lib/tomcat7/webapps
+cp -R /home/ubuntu/Wildbook_javaTweetBot/target/wildbook-6.0.0-EXPERIMENTAL/ /var/lib/tomcat7/webapps/wildbook/
+#sudo chmod 777 /var/lib/tomcat7/webapps/wildbook.war
 
 cd /var/lib; sudo chmod -R 777 tomcat7/; sudo chown -R tomcat7 tomcat7/; sudo chgrp -R tomcat7 tomcat7/
 
