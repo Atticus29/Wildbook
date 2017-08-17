da7e3efa87f73d4bf89a2f4da18055975bf1c212
Whaleshark Dev <info@whaleshark.org>
2017-08-17 18:08:32 +0000

    fix merge conflict with tweetSuite.sh

--- tweetSuite.sh
+++ tweetSuite.sh
@@ -1,5 +1,4 @@
 git stash
-git stash
 current_branch=git branch| awk -F "[ ',]+" '/*/{print $2}'
 echo $current_branch
 git stash pop
@@ -10,4 +9,4 @@ sudo service tomcat7 restart
 curl http://34.213.108.79/tweetImageTest.jsp >> buildOutput.txt 2>>buildError.txt
 sudo service tomcat7 stop
 git checkout $current_branch
-git pop
+git stash pop
