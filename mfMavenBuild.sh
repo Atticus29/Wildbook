6382b384b64db0086704ea57f6816fc1eb15fd84
Mark <mark.aaron.fisher@gmail.com>
2017-08-17 15:49:02 -0700

    fix typo in mfMavenBuild in counterTweet branch

--- mfMavenBuild.sh
+++ mfMavenBuild.sh
@@ -11,8 +11,8 @@ rm -rf /usr/local/apache-tomcat-7.0.79/logs/catalina.out
 
 mvn clean install -DskipTests -Dmaven.javadoc.skip=true && cp /Users/mf/Desktop/temp/Wildbook_javaTweetBot/target/wildbook-6.0.0-EXPERIMENTAL.war /usr/local/apache-tomcat-7.0.79/webapps/wildbook.war
 
-cp ~/Desktop/temp/Wildbook_javaTweetBot/tiwtter.properties /Desktop/temp/Wildbook_javaTweetBot/target/classes/bundles/
-cp ~/Desktop/temp/Wildbook_javaTweetBot/tiwtter.properties /Desktop/temp/Wildbook_javaTweetBot//target/wildbook-6.0.0-EXPERIMENTAL/WEB-INF/classes/bundles/
-cp ~/Desktop/temp/Wildbook_javaTweetBot/tiwtter.properties /Users/mf/Applications/tomcat/webapps/wildbook/WEB-INF/classes/bundles/
-cp ~/Desktop/temp/Wildbook_javaTweetBot/tiwtter.properties /Users/mf/Applications/tomcat/webapps/wildbook_data_dir/WEB-INF/classes/bundles/twitter.properties
+cp ~/Desktop/temp/Wildbook_javaTweetBot/twitter.properties /Desktop/temp/Wildbook_javaTweetBot/target/classes/bundles/
+cp ~/Desktop/temp/Wildbook_javaTweetBot/twitter.properties /Desktop/temp/Wildbook_javaTweetBot/target/wildbook-6.0.0-EXPERIMENTAL/WEB-INF/classes/bundles/
+cp ~/Desktop/temp/Wildbook_javaTweetBot/twitter.properties /Users/mf/Applications/tomcat/webapps/wildbook/WEB-INF/classes/bundles/
+cp ~/Desktop/temp/Wildbook_javaTweetBot/twitter.properties /Users/mf/Applications/tomcat/webapps/wildbook_data_dir/WEB-INF/classes/bundles/twitter.properties
 
