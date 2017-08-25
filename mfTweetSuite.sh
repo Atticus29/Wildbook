2ffa180b828fbd33e45995f842361f60f4738e26
Mark <mark.aaron.fisher@gmail.com>
2017-08-25 15:51:52 -0700

    add new tests to tweetImageTest and chance twitter properties with new API key info. Will also fix all of the twitter shutting us down nonsense

---
+++ mfTweetSuite.sh
@@ -0,0 +1,14 @@
+#Warning: just use this as a model. Don't actually run
+
+# git stash
+# current_branch=git branch| awk -F "[ ',]+" '/*/{print $2}'
+# echo $current_branch
+# git stash pop
+# git checkout counterTweet
+# sudo service tomcat7 stop
+# sudo ./ami.sh>buildOutput.txt 2>buildError.txt
+# sudo service tomcat7 restart
+curl http://localhost:8080/wildbook/tweetImageTest.jsp >> buildOutput.txt 2>>buildError.txt
+# sudo service tomcat7 stop
+# git checkout $current_branch
+# git stash pop
