5d4e9c94a5e12c50bfeeb69f460a1a57e0e4ce04
Mark <mark.aaron.fisher@gmail.com>
2017-09-30 14:17:26 -0700

    change FileInputStream to FileOutputStream in writeToFile method

--- src/main/java/org/ecocean/Util.java
+++ src/main/java/org/ecocean/Util.java
@@ -596,7 +596,7 @@ public class Util {
     public static void writeToFile(String data, String path) throws FileNotFoundException {
       PrintWriter out = null;
       try{
-        out = new PrintWriter(new FileInputStream(path, false));
+        out = new PrintWriter(new FileOutputStream(path, false));
         out.println(data);
         out.close();
       } catch(Exception e){
