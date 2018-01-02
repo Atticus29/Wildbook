e2e488562e52bc09a28b67f7b53443bbaae05c25
uidev generic <info@wildme.org>
2018-01-02 23:55:00 +0000

    add minimal_db.sql, make slight alterations to ami.sh, and capture other unclear changes in various docker and stanford nlp files that, when diffed, did not yield a lot of insightful information

--- ami.sh
+++ ami.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cp /home/ubuntu/Wildbook_javaTweetBot/twitter.properties /home/ubuntu/Wildbook_javaTweetBot/src/main/resources/bundles/twitter.properties
-cp /home/ubuntu/Wildbook_javaTweetBot/twitter.properties /data/whaleshark_data_dirs/shepherd_data_dir/WEB-INF/classes/bundles/
+sudo cp /home/ubuntu/Wildbook_javaTweetBot/twitter.properties /home/ubuntu/Wildbook_javaTweetBot/src/main/resources/bundles/twitter.properties
+#sudo cp /home/ubuntu/Wildbook_javaTweetBot/twitter.properties /data/whaleshark_data_dirs/shepherd_data_dir/WEB-INF/classes/bundles/
+sudo cp /home/ubuntu/Wildbook_javaTweetBot/twitter.properties /data/wildbook_data_dir/WEB-INF/classes/bundles/
 
 rm -rf /var/lib/tomcat7/webapps/wildbook.war
 
@@ -9,11 +10,23 @@ rm -rf /var/lib/tomcat7/webapps/wildbook
 
 rm -rf /var/lib/tomcat7/logs/catalina.out
 
+rm -rf /opt/tomcat/webapps/wildbook.war
+
+rm -rf /opt/tomcat/webapps/wildbook
+
+rm -rf /opt/tomcat/logs/catalina.out
+
+
 mvn clean install -DskipTests -Dmaven.javadoc.skip=true
-#cp /home/ubuntu/Wildbook_javaTweetBot/target/wildbook-6.0.0-EXPERIMENTAL.war /var/lib/tomcat7/webapps/wildbook.war
+cp /home/ubuntu/Wildbook_javaTweetBot/target/wildbook-6.0.0-EXPERIMENTAL.war /var/lib/tomcat7/webapps/wildbook.war
+cp /home/ubuntu/Wildbook_javaTweetBot/target/wildbook-6.0.0-EXPERIMENTAL.war /opt/tomcat/webapps/wildbook.war
 sudo mkdir /var/lib/tomcat7/webapps
+sudo mkdir /opt/tomcat/webapps
 cp -R /home/ubuntu/Wildbook_javaTweetBot/target/wildbook-6.0.0-EXPERIMENTAL/ /var/lib/tomcat7/webapps/wildbook/
-#sudo chmod 777 /var/lib/tomcat7/webapps/wildbook.war
+cp -R /home/ubuntu/Wildbook_javaTweetBot/target/wildbook-6.0.0-EXPERIMENTAL/ /opt/tomcat/webapps/wildbook/
+sudo chmod 777 /var/lib/tomcat7/webapps/wildbook.war
+sudo chmod 777 /opt/tomcat/webapps/wildbook.war
 
 cd /var/lib; sudo chmod -R 777 tomcat7/; sudo chown -R tomcat7 tomcat7/; sudo chgrp -R tomcat7 tomcat7/
+cd /opt/; sudo chmod -R 777 tomcat7/; sudo chown -R tomcat7 tomcat7/; sudo chgrp -R tomcat7 tomcat7/
 
